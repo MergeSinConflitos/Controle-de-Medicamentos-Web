8ee83b45761715c88501127802dbdf39795287f1
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-06-11 03:18:04 -0300

    add Dtos and Servico

---
+++ ControleDeMedicamentos.WebApplication/ModuloFuncionarios/Aplicacao/FuncionarioDto.cs
@@ -0,0 +1,25 @@
+namespace ControleDeMedicamentos.WebApplication.ModuloFuncionarios.Aplicacao;
+
+public record ListarFuncionariosDto(
+Guid Id, 
+string Nome, 
+string Telefone, 
+string Cpf);
+
+public record CadastrarFuncionarioDto(
+string Nome, 
+string Telefone,
+string Cpf
+);
+
+public record EditarFuncionariosDto(
+Guid Id, 
+string Nome, 
+string Telefone, 
+string Cpf);
+
+public record DetalhesFuncionariosDto(
+Guid Id, 
+string Nome, 
+string Telefone, 
+string Cpf);
